f3561a6ad740bbc9cd0aad766a53e095dd569dbc
r1413 <raydunc15@gmail.com>
2017-03-14 16:26:06 -0600

    Added Vehicle Class

---
+++ Vehicle.py
@@ -0,0 +1,32 @@
+import random
+import graph
+
+vehicleType enum
+	
+
+class Vehicle:
+	def __init__(self):
+		#Do something
+		self._length = 0
+		self._vMax = 0
+		self._v = 0
+		self._acceleration = 0
+		self._deceleration = 0
+	
+	def findPath():
+		#Do something
+		
+	def getLength():
+		return self._length
+	
+	def getVMax():
+		return self._vMax
+		
+	def getV():
+		return self._v
+		
+	def getAcceleration():
+		return self._acceleration
+		
+	def getDeceleration():
+		return self._deceleration
