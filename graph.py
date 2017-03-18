7308b66745581cc0074980242b458715d89e98a1
happymuffen <yakurbe0112@gmail.com>
2017-03-18 21:47:04 +0100

    added edges and print

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib
 size=20
 nodes[size]
 
@@ -14,16 +15,40 @@ class node():
 	getlocation(self):
 		return self.location
 	
-	addedge(self,node):
-		if self.aloc==self.edgenumber-1:
+	addedge(self,node,edges):
+		if self.aloc==self.edgenumber:
+			return 0
+		if node.aloc==node.edgenumber:
 			return 0
 		for i in range(self.aloc):
 			if edges[i]==node:
 				return 0
+		if node==self:
+			return 0
 		
 		edges[self.aloc]=node
+		node.edges[node.aloc]
+		edges.append([self,node])
+		self.aloc+=1
+		node.aloc+=1
 		return 1
-			
+
+
+		
+def sortnode(node,nodes):
+	i=0
+	dist[nodes.len]
+	for n in nodes:
+		dist[i]=[n,(n.location[0]-node.location[0])**2+(n.location[1]-node.location[1])**2]
+		i+=1
+	i=0
+	out[nodes.len]
+	for n in (sorted(dist,key=lambda dist: dist[1]]):
+		out[i]=n[0]
+		i++
+	return out
+	
+edges=[]
 thrs=0
 for i in range(size):#build all nodes
 	edgenumber=random.randint(3,4)
@@ -33,4 +58,14 @@ for i in range(size):#build all nodes
 		else:
 			edgenumber=3
 	nodes[i]=node(edgenumber)
+for n in nodes:
+	dist[nodes.len]
+	if n.edgenumber>0:
+		dist=sortnode(n,nodes)
+	while n.edgenumber>0:
+		i=0
+		while !(n.addedge(dist[i]),edges):
+			i+=1
 
+for e in edges:
+	print str(e[0].getlocation())+", "+str(e[1].getlocation())
