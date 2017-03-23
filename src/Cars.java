b55bc37a73ffd1819d47ee168329d6f59e421ccf
happymuffen <yakurbe0112@gmail.com>
2017-03-23 23:36:04 +0100

    added car class

---
+++ src/Cars.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ben
+ */
+public class Cars {
+    Intersection src;
+    Intersection dst;
+    Intersection[] path;
+    dijkstra(CityMap map){
+    	
+    }
+    Cars(CityMap map){
+        src=map.getrandom(); 
+        dst=map.getrandom();
+        while(src==dst){
+            dst=map.getrandom()
+        }
+        
+        
+    }
+}
