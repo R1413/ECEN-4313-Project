69a905c92dd3b6359523730c0505b3f27379ced3
happymuffen <yakurbe0112@gmail.com>
2017-04-04 05:48:12 +0100

    altered Road class in CityMap. Finished adding Djiksta's algorithm. still need to test.

--- src/CityMap.java
+++ src/CityMap.java
@@ -14,12 +14,14 @@ public class CityMap {
 		private int speedLimit;
 		private int length; 
 		private Intersection end;
+		private Intersection start;
 		
-		public Road(int speedLimit, int length, Intersection end){
+		public Road(int speedLimit, int length, Intersection end, Intersection start){
 			this.speedLimit = speedLimit;
 			this.length = length;
-			this.end = new Intersection();
+			//this.end = new Intersection();
 			this.end = end;
+			this.start=start;
 		}
 	}
 	
@@ -34,11 +36,81 @@ public class CityMap {
 	public void addIntersection(String edge){
 		//Use formatting of string to build connections
 	}
-	Intersection[] public dijkstra(Intersection src,Intersection dst){
-		public class dnode{
-			Intersection i=new ArrayList();
+	List<Intersection> public dijkstra(Intersection src,Intersection dst){
+		//lets try again
+		private class Interwrap{
+			List<Intersection> path;
+			static Intersection inter;
 			int dist=Integer.MAX_VALUE;
-			List path;
+			Interwrap(Intersection i){
+				inter=i;
+			}
+			
+		}
+		Interwrap current;
+		//ArrayList<Intersection> output=new ArrayList<Intersection>;
+		//List<Interwrap> checked=new List<Interwrap>;
+		List<Interwrap> unchecked=new List<Interwrap>;
+		
+		void init(){//initializes dijkstra's algorithm
+			for(Intersection inter: this.intersections){
+				unchecked.add(new Interwrap(inter));
+				if inter==src{
+					int index=unchecked.size()-1;
+					unchecked.get(index).dist=0;
+					current=inter;
+				}
+			}
+		}
+		void neighbor(Interwrap current,Interwrap n){
+			//takes total distance to neighboring node and neighboring node
+			if n.dist>current.dist{
+				n.dist=dist;
+				n.path= new List(current.path);
+				n.path.add(current.inter);
+			}
+		}
+		
+		void setCurrent(){//adds neigbors of the smallest unchecked node
+			
+			for(Road road:current.inter.roads){//check neighbors of current
+				Intersection i;
+				if (road.end==current.inter){
+					i= road.start;
+				}
+				else{
+					i= road.end;
+				}
+				for(Interwrap n:unchecked){
+					if n.inter==i{
+						neighbor(current,inter);
+					}
+				}
+			}
+			
+			unchecked.remove(current);
+			
+			//set current to next closest unchecked
+			int index=-1;
+			int close=Integer.MAX_VALUE;
+			for(Interwrap inter:unchecked){
+				if (inter.dist<close){
+					close=inter.dist;
+					index=unchecked.indexOf(inter);
+				}
+			}
+			current=unchecked.get(index);
+		}
+		while(current.inter!=dst){
+			setCurrent();
+		}
+		current.path.add(current.inter);
+		return current.path;
+		
+	}
+		/*public class dnode{
+			Intersection i=new Intersection;
+			int dist=Integer.MAX_VALUE;
 			dnode(Intersection inter){
 				this.i=inter;
 			}
@@ -58,7 +130,20 @@ public class CityMap {
 			return i;
 		}
 		void neighbors(dnode node){
-			for()
+			for(Road r:node.i.road){
+				boolean b=true;
+				for(dnode node: checked){
+					if(r.end==node.i){
+						b=false;
+						break;
+					}
+				}
+				if(b){
+					if (node.dist+r.length<r.end.dist){
+						
+					}
+				}
+			}
 		}
 		List unchecked=new Arraylist();
 		List checked=new Arraylist();
@@ -67,7 +152,7 @@ public class CityMap {
 			if i==src{
 				node.dist=0;
 			}
-			unchecked.add(0,node);
+			unchecked.add(node);
 		}
 		while(!unchecked.isEmpty()){
 			int index=getNearestIndex(unchecked,src);
@@ -75,7 +160,7 @@ public class CityMap {
 			checked.add(0,node);
 		}
 		
-	}
+	}*/
 	
 	
 	
