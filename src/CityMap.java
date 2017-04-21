9935b85ed2d75e6a5d1243be8edc30a7f3612f0c
happymuffen <yakurbe0112@gmail.com>
2017-04-21 19:34:42 +0100

    made constructors for roads intersection and citymap

--- src/CityMap.java
+++ src/CityMap.java
@@ -3,12 +3,35 @@ import java.util.*;
 
 
 public class CityMap {
-	public class Intersection{
-		private Road roads[];
-		private String name;
+	private class Intersection{
+		private Road roadlist[];
+		private int r=0;
+		private String name="";// "42,-13"
 		
-		public Intersection(){
+		private Intersection(String a){
+			roadlist= new Road[4];
+			name=a;
+			intersections[internum]=this;
+			internum=internum+1;
 			//Do something?
+			System.out.print("made "+name+"\n");
+		}
+		private void addRoad(Intersection end){
+			int speedlimit=25;
+			int[] coords=new int[4];
+			String n= new String(name);
+			String[] split= n.split(",");
+			coords[0]=Integer.parseInt(split[0]);
+			coords[1]=Integer.parseInt(split[1]);
+			n=new String(end.name);
+			split=n.split(",");
+			coords[2]=Integer.parseInt(split[0]);
+			coords[3]=Integer.parseInt(split[1]);
+			int length=(int) Math.sqrt((coords[0]-coords[2])^2+(coords[1]-coords[3]^2));System.out.print("making road from "+name+" to "+end.name+"\n");
+			roadlist[r]=new Road(speedlimit,length,end);
+			roads[roadnum]=roadlist[r];
+			r=r+1;
+			roadnum=roadnum+1;
 		}
 	}
 	public class Road{
@@ -22,12 +45,14 @@ public class CityMap {
 			this.length = length;
 			//this.end = new Intersection();
 			this.end = end;
-			
+			//System.out.print("made road from "+name+" to "+end.name+"\n");
 		}
 	}
-	
-	private Intersection intersections[];
-	private Road roads[];
+	public int MAX_INTERSECTIONS=100;
+	private Intersection intersections[]=new Intersection[MAX_INTERSECTIONS];
+	private int internum=0;
+	private Road roads[]=new Road[MAX_INTERSECTIONS*8];
+	private int roadnum=0;
 	//private Map<(int,int),Intersection> lookup;
 	
 	public CityMap(String mapFile){
@@ -50,9 +75,18 @@ public class CityMap {
 				//System.out.println(splitLine[0]);
 				for(int i = 1; i < 5; i++){
 					digits[i-1] = Integer.parseInt(splitLine[i]);
-				}
+				}//Lines split into digits
 				
-				//Lines split into digits
+				String name1=Integer.toString(digits[0]).concat(",").concat(Integer.toString(digits[1]));//name of intersections
+				Intersection a=findInter(name1);
+				String name2=Integer.toString(digits[2]).concat(",").concat(Integer.toString(digits[3]));
+				Intersection b=findInter(name2);
+				
+				if(a==null)a=new Intersection(name1);//allocate new intersections
+				if(b==null)b=new Intersection(name2);
+				
+				a.addRoad(b);//make roads
+				b.addRoad(a);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -60,6 +94,17 @@ public class CityMap {
 		}
 	}
 	
+	public Intersection findInter(String name){
+		System.out.print("finding "+name+"\n");
+		if(internum==0)return null;
+		for(Intersection i:intersections){
+			if(i==null)return null;
+			if(i.name.equals(name)) return i;
+		}
+		//shouldnt get here
+		return null;
+	}
+
 	public void addIntersection(String edge){
 		//Use formatting of string to build connections
 	}
