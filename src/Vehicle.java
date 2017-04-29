16b019b871c5bedbaafacaf7780a05601d073f80
happymuffen <yakurbe0112@gmail.com>
2017-04-29 23:54:14 +0100

    let me try that again

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -7,14 +7,11 @@ public abstract class Vehicle {
 	private int dec;
 	private Stack<String> path;
 	private String home;
-	private String[] dests;
-	private Map<String,Intersection> map;
 	
 	private VehicleType vehicleType;
-	public Vehicle(Map<String,Intersection>mapIn){
+	public Vehicle(String h){
 		//Do Something
 		this.vehicleType = VehicleType.Car;
-		map=mapIn;
 		
 		//Choose vehicle attributes based on vehicle type
 		if(vehicleType == VehicleType.Car){
@@ -24,12 +21,16 @@ public abstract class Vehicle {
 		}
 		
 		//Chose home and some number of destinations
-		Random random = new Random();
+		home=h;
+		/*Random random = new Random();
 		home=map.get(random.nextInt(map.size())).getName();
 		dests=new String[random.nextInt(map.size())];
 		for(String k:dests){
 			k=map.get(random.nextInt(map.size())).getName();
 		}
+		*/
+		
+		//Fill path stack;
 		
 		//Choose vehicle behavior based on driving style
 		//added later
